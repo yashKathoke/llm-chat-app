5b87cd21e14a92aebff237bc76ac9c01a7080a75
yash kathoke <kathokeyash68@gmail.com>
2025-06-20 16:19:37 +0530

    fixed typo in config.py

--- backend/config.py
+++ backend/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 
-FRONTEND_URL = os.getenv("FRONTEND_URL") | "*"
+FRONTEND_URL = os.getenv("FRONTEND_URL") or "*"
