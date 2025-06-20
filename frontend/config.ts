1547deae1d3eb78fb2dc2b06fa8a424ae350b78c
yash kathoke <kathokeyash68@gmail.com>
2025-06-20 13:54:52 +0530

    added backend url config in env for aws use

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,1 +1,7 @@
-export const BACKEND_URL = "http://127.0.0.1:8000"
+import dotenv from 'dotenv';
+dotenv.config();
+
+const  BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8000";
+
+
+export {BACKEND_URL}
